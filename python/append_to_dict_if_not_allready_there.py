1144caa2780420d6b3e7ccee711b696b5bfb2ba0
ludeeus <joasoe@gmail.com>
2018-12-24 18:30:43 +0100

    linting

--- python/append_to_dict_if_not_allready_there.py
+++ python/append_to_dict_if_not_allready_there.py
@@ -1,11 +1,13 @@
 
-dict = ['192.168.2.1','192.168.2.2']
-new_keys = ['192.168.1.1','192.168.2.2','192.168.1.3','192.168.2.4','192.168.1.5','192.168.1.6']
+"""Append dict to dict."""
+DICT = ['192.168.2.1', '192.168.2.2']
+NEW_KEYS = ['192.168.1.1', '192.168.2.2', '192.168.1.3',
+            '192.168.2.4', '192.168.1.5', '192.168.1.6']
 
-for key in new_keys:
-  if key not in dict:
-    print('adding' + key)
-    dict.append(key)
-  else:
-    print('skipping' + key)
-print(dict)
+for key in NEW_KEYS:
+    if key not in DICT:
+        print('adding' + key)
+        DICT.append(key)
+    else:
+        print('skipping' + key)
+print(DICT)
