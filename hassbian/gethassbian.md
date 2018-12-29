5014fbf74ecdd199d3c5cca8ddeb2805223b1c6e
ludeeus <joasoe@gmail.com>
2018-12-29 13:32:08 +0100

    gethassbian

---
+++ hassbian/gethassbian.md
@@ -0,0 +1,23 @@
+# Get Hassbian
+
+Simple script to install the [tools used in Hassbian][hassbian-scripts].
+
+```bash
+curl -sSL https://raw.githubusercontent.com/ludeeus/ROOT/master/hassbian/gethassbian.sh | sudo bash -
+```
+
+The **only** supported way of installing this is using the [Hassbian image.][image]  
+But some have had luck installing it on other distros.  
+**No** support will be given for unsupported distros.
+
+**Tested on:**  
+
+- Debian 9
+- Raspbian Strtech (minimal)
+- Raspbian Stretch (w/GUI)
+- Ubuntu server 18.04
+
+[Scripts included.][hassbian-scripts]
+
+[hassbian-scripts]: https://github.com/home-assistant/hassbian-scripts
+[image]: https://www.home-assistant.io/docs/installation/hassbian/
---
+++ hassbian/gethassbian.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# This script will install hassbian-config on the unit.
+#
+
+if [ "$(id -u)" != "0" ]; then
+echo "This script must be run with sudo. Use \"sudo ${0} ${*}\"" 1>&2
+exit 1
+fi
+
+echo "Updating distro..."
+apt update
+apt upgrade -y
+apt dist-upgrade
+
+echo "Installing additional software..."
+apt-get install -y git python3 curl python3-venv python3-pip bluetooth libbluetooth-dev rng-tools htop tmux avahi-daemon libtool autoconf rfkill apt-transport-https
+pip3 install --upgrade virtualenv
+
+echo "Adding homeassistant user and install dir..."
+addgroup homeassistant
+useradd -u 1001 -g homeassistant -rm homeassistant
+mkdir /srv/homeassistant
+chown homeassistant:homeassistant -R /srv/homeassistant
+chown homeassistant:homeassistant -R /home/homeassistant
+usermod -G dialout -a homeassistant
+usermod -G i2c -a homeassistant
+usermod -G gpio -a homeassistant
+usermod -G spi -a homeassistant
+
+
+echo "Downloading and installing hassbian-config..."
+echo "deb [trusted=yes] https://gitlab.com/hassbian/repository/raw/master stretch main" | sudo tee /etc/apt/sources.list.d/hassbian-config.list
+apt-get update
+apt install -y hassbian-scripts
+
+
+echo "Installation complete..."
